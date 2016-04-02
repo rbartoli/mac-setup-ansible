3d4aab6305ae3ca2fa4f29083d8fbdf697355a98
Riccardo Bartoli <rbartoli@salmon.com>
2016-04-03 00:33:16 +0100

    Enforce sudo and enable verbosity

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &÷
+
 # Update all OSX packages
 sudo softwareupdate -i -a
 
@@ -30,4 +36,4 @@ fi
 export PATH=/usr/local/bin:$PATH
 
 # Start Ansible
-ansible-playbook -K playbook.yml
+ansible-playbook -K -v playbook.yml
